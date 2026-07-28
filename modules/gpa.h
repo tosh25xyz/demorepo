4aa01e17165f11461517d0eb3c0fe2314f530d0b
tosh25xyz <toshrif1986@gmail.com>
2026-07-29 01:27:23 +0600

    Declared expected CGPA calculator

--- modules/gpa.h
+++ modules/gpa.h
@@ -10,5 +10,7 @@ extern const char *gradeLetters[];
 double getGradePoint(CourseResult result);
 char *getLetterGrade(CourseResult result);
 double calculateGPA(CourseResult results[], int n_results);
+double calculateExpectedCGPA(double current_cgpa, double completed_credits,
+                             double expected_gpa, double future_credits);
 
 #endif
